c8de4cc96f83a5e1858e76a0defe37ce892dd90b
KobeHaukap <kobe.haukap@gmail.com>
2023-04-27 10:25:05 -0500

    start

---
+++ DataValidation.py
@@ -0,0 +1,26 @@
+"My data validator"
+
+# imports
+import re
+
+# metadata for authorship information
+__author__ = 'Kobe Haukap'
+__version__ = '1.0'
+__date__ ='2023.04.27'
+__status__ = 'development'
+
+def validate_id():
+    re.search([0-100], )
+def validate_name():
+
+def validate_email():
+
+def validate_phone():
+
+def validate_date():
+
+def validate_time():
+
+
+def __main__():
+
