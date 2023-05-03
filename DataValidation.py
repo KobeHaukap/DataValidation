b8d271d6315d79c2109c163316eb947592a08156
KobeHaukap <kobe.haukap@gmail.com>
2023-05-02 22:10:35 -0500

    added logic to validators, started file creation.

--- DataValidation.py
+++ DataValidation.py
@@ -1,4 +1,4 @@
-"My data validator"
+"My Python Data Validator"
 
 # imports
 import re
@@ -9,18 +9,66 @@ __version__ = '1.0'
 __date__ ='2023.04.27'
 __status__ = 'development'
 
-def validate_id():
-    re.search([0-100], )
-def validate_name():
 
-def validate_email():
+def validate_id(id):
 
-def validate_phone():
+    regex_id = '^[0-9]+$'
+    if re.compile(regex_id, id):
+        return ""
+    else:
+        return "I"
 
-def validate_date():
+    
+def validate_name(name):
 
-def validate_time():
+    parts = name.split()
+    if len(parts) == 2:
+        return ""
+    else:
+        return "N"
+
+
+def validate_email(email):
+
+    regex_email = r'^[\w-]+@\w+\.edu$'
+    if re.compile(regex_email, email):
+        return ""
+    else:
+        return "E"
+
+
+def validate_phone(number):
+
+    regex_phone = r'\d{3}-\d{3}-\d{4}'
+    if re.match(regex_phone, number):
+        return ""
+    else:
+        return "P"
+
+
+def validate_date(date):
+
+    regex_date = "%d-%m-%Y"
+    if re.compile(regex_date, date):
+        return ""
+    else:
+        return "D"
+
+
+def validate_time(time):
+
+    regex_time = "([01]?[0-9]|2[0-3]):[0-5][0-9]"
+    if re.compile(regex_time, time):
+        return ""
+    else:
+        return "T"
+
+
+def process_file(input_data, valid_data, invalid_data):
+
+    with open('input_data.txt', 'a', newline='\n') as input_data
 
 
 def __main__():
+    process_file()
 
